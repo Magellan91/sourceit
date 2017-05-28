140a6345bbaad1030daae5b9c44aa6b79c008db5
Magellan91 <muqpka1991@gmail.com>
2017-05-28 12:14:01 +0300

    Homework 13

--- 13/13.1.php
+++ 13/13.1.php
@@ -3,6 +3,40 @@
 <head>
     <meta charset="UTF-8">
     <title>Вод данных</title>
+    <script
+            src="https://code.jquery.com/jquery-3.2.1.min.js"
+            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+            crossorigin="anonymous"></script>
+    <style type="text/css">
+        body {
+            background: #fffbfb;
+            font-size: 14px;
+        }
+
+        form {
+            margin: 0 auto;
+            padding: 10px;
+            background: #a3a2ff;
+            width: 600px;
+            border-radius: 3px;
+            color: #000000;
+            box-shadow: 0 0 10px #000000;
+
+        }
+
+        #out {
+            margin: 0 auto;
+            color: #000000;
+            width: 550px;
+        }
+        .error {
+            color: red;
+        }
+
+        .hide {
+            display: none;
+        }
+    </style>
     <script type="text/javascript">
         $(function () {
             var form = $("#user_form");
@@ -33,15 +67,69 @@
         });
     </script>
 </head>
-<body><pre>
+<body>
+
+<pre>
 <form action="" method="post" id="user_form">
     <div id="out">Имя:       <input name="first_name" value="" id="first_name"></div>
+     <div class="error hide" id="first_name_error">Вопрос должно быть больше 2х символов</div>
     <div id="out">Фамилия:   <input name="last_name" value="" id="last_name"></div>
+     <div class="error hide" id="last_name_error">Вопрос должно быть больше 2х символов</div>
     <div id="out">Компания:  <input name="company" value="" id="company"></div>
+     <div class="error hide" id="company_error">Вопрос должно быть больше 2х символов</div>
     <div id="out">Город:     <input name="city" value="" id="city"></div>
+     <div class="error hide" id="city_error">Вопрос должно быть больше 2х символов</div>
     <div id="out">Страна:    <input name="country" value="" id="country"></div>
+     <div class="error hide" id="country_error">Вопрос должно быть больше 2х символов</div>
     <div>        <input type="submit" value="Отправить"></div>
-    <?php include("Serv.php"); ?>
+
+<script type="text/javascript">
+    $(function () {
+        var form = $("#user_form");
+        form.on('submit', onSubmit);
+        function onSubmit(event) {
+            var f_name = $('#first_name');
+            var l_name = $('#last_name');
+            var compan = $('#company');
+            var cit = $('#city');
+            var countr = $('#country');
+            var valid = true;
+            if (f_name.val().length <= 2) {
+                valid = false;
+                $('#first_name_error').removeClass('hide');
+            } else {
+                $('#first_name_error').addClass('hide');
+            }
+            if (l_name.val().length <= 2) {
+                valid = false;
+                $('#last_name_error').removeClass('hide');
+            } else {
+                $('#last_name_error').addClass('hide');
+            }
+            if (compan.val().length <= 2) {
+                valid = false;
+                $('#company_error').removeClass('hide');
+            } else {
+                $('#company_error').addClass('hide');
+            }
+            if (cit.val().length <= 2) {
+                valid = false;
+                $('#city_error').removeClass('hide');
+            } else {
+                $('#city_error').addClass('hide');
+            }
+            if (countr.val().length <= 2) {
+                valid = false;
+                $('#country_error').removeClass('hide');
+            } else {
+                $('#country_error').addClass('hide');
+            }
+            if (!valid) {
+                event.preventDefault();
+            }
+        }
+    });
+</script>
 </pre>
 </form>
 
--- 13/Serv.php
+++ 13/Serv.php
@@ -1,12 +1,11 @@
 <?php
-if (!empty($_POST['first_name'])&&!empty($_POST['last_name'])&&!empty($_POST['company'])&&!empty($_POST['city'])&&!empty($_POST['country'])) {
-    $first_name = $last_name= $company=$city=$country = null;
-    $first_name=$_POST['first_name'];
-    $last_name=$_POST['last_name'];
-    $company=$_POST['company'];
-    $city=$_POST['city'];
-    $country=$_POST['country'];
-    $created=date('d.m.y');
+if (!empty($_POST['first_name']) && !empty($_POST['last_name']) && !empty($_POST['company']) && !empty($_POST['city']) && !empty($_POST['country'])) {
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $company = $_POST['company'];
+    $city = $_POST['city'];
+    $country = $_POST['country'];
+    $created = date('d.m.y H:i:s');
     include('connect.php');
     $stmt = mysqli_prepare($mysqli, "INSERT INTO `supplliers` (first_name, last_name, company, city, country, created) VALUES (?, ?, ?, ?, ?, ?)");
     mysqli_stmt_bind_param($stmt, 'ssssss', $first_name, $last_name, $company, $city, $country, $created);
@@ -16,8 +15,27 @@ if (!empty($_POST['first_name'])&&!empty($_POST['last_name'])&&!empty($_POST['co
     $true = ['success' => true];
     echo json_encode($true);
 } else {
-    $error = ['success' => false, 'errors' => ['question' => 'Field is empty']];
+    $error=null;
+    if (empty($_POST['first_name'])) {
+        $error = ['success' => false,['errors_first_name'  => 'first_name is empty']];
         echo json_encode($error);
+    }
+    if (empty($_POST['last_name'])) {
+        $error =['success' => false,['errors_last_name' => 'last_name is empty']] ;
+        echo json_encode($error);
+    }
+    if (empty($_POST['company'])) {
+        $error = ['success' => false,['errors_company' => 'company is empty']] ;
+        echo json_encode($error);
+    }
+    if (empty($_POST['city'])) {
+        $error =['success' => false,['errors_city' =>  'city is empty']] ;
+        echo json_encode($error);
+    }
+    if (empty($_POST['country'])) {
+        $error = ['success' => false,['errors_country' =>  'country is empty']];
+        echo json_encode($error);
+    }
 }
 
 ?>
